835b2327534affe6f6802d1a4e42c02eb1cd8b6a
TharukaSomathilaka <shaniththaruka@gmail.com>
2026-06-27 14:50:03 +0530

    Operators

---
+++ js/app.js
@@ -0,0 +1,47 @@
+//let name = "Amarapala";
+
+//let age= 18;
+
+//console.log(name);
+//console.log(age);
+
+//alert("Hello iCET");
+
+let name = "Amal";
+
+console.log(typeof name);
+
+let age = 18;
+let isAfterAL = false;
+let customers = [];
+let customer = {};
+let salary = 7500.0;
+
+console.log(typeof age);
+console.log(typeof isAfterAL);
+console.log(typeof customers);
+console.log(typeof customer);
+console.log(typeof salary);
+
+let x = 10;
+let y = "10";
+
+console.log(x===y);
+
+//  logical operators
+
+//  && | || | !
+
+// if (!false) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
+
+
+
+
+
+
+
